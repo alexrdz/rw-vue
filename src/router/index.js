ab53b924a56a7e39bd71ee47c69cd4f0ec512a9d
AlexRdz <drinkhorchata@gmail.com>
2021-10-10 15:42:54 -0600

    finish v1

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
-import EventList from "../views/EventList.vue";
 import About from "../views/About.vue";
+import EventList from "../views/EventList.vue";
+import EventDetails from "../views/EventDetails.vue";
 
 const routes = [
   {
@@ -11,10 +12,13 @@ const routes = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: About,
+  },
+  {
+    path: "/event/:id",
+    name: "EventDetails",
+    props: true,
+    component: EventDetails,
   },
 ];
 
